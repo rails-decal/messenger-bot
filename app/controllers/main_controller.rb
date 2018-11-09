b91c8f6cfc65aed0ccb7cb79caec6f2a50df6b4d
Ethan Lee <hello@ethanl.ee>
2018-11-08 17:16:46 -0800

    Remove solution

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,11 +1,25 @@
 class MainController < ApplicationController
   skip_before_action :verify_authenticity_token, :only => [:receive]
+  REPLACE_THIS = "ignore" # ignore this line :)
+
   def index
     render plain: params["hub.challenge".to_sym]
   end
 
+  # This method gets called when a message arrives to your Facebook page.
   def receive
-    send_message("ok", params[:entry].first[:messaging].first[:sender][:id])
+    #
+    # Call send_message here with two parameters:
+    #
+    # 1) The text of the response
+    # 2) The sender ID from the params of this request. Check out your
+    #    ngrok dashboard to see how to grab this ID, and if you're having
+    #    trouble, check out the lab guide.
+    send_message(
+      # Fill in the two parameters described above here!
+    )
+
+    # We send back an arbitrary successful response to Facebook
     render json: { ok: true }
   end
 
@@ -13,14 +27,18 @@ class MainController < ApplicationController
   def send_message(text, conversation_id)
     conn = Faraday.new
     conn.post(
-      "https://graph.facebook.com/v2.6/me/messages?access_token=#{Rails.application.credentials.facebook_token}",
+      # Replace the API URL. Make sure you use your access token in this URL!
+      # It is available to you as Rails.application.credentials.facebook_token.
+      "the API URL here", 
       {
         "messaging_type": "RESPONSE",
         "recipient": {
-          "id": conversation_id
+          # Your code here - it should come from the arguments to this function
+          "id": REPLACE_THIS
         },
-        "message":{
-          "text": text
+        "message": {
+          # Your code here - it should also come from the arguments to this function
+          "text": REPLACE_THIS
         }
       }
     )
